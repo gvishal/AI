e43cc2f98ed0e8e492049dfd1c5b8a5b3e88610a
Vishal Gupta <vishal.vgu+github@gmail.com>
2015-02-08 21:55:11 +0530

    A1 function def added

---
+++ A1/ut3.py
@@ -0,0 +1,26 @@
+'''Ultimate Tic Tac Toe'''
+
+class T3(object):
+    '''Classname will be provided later'''
+    def __init__(self):
+        super(T3, self).__init__()
+
+    def move(self, current_board_game, board_stat, move_by_opponent, flag):
+        '''Return tuple of next move (row, column).Check if next move is valid or not'''
+        pass
+
+    def utility(self, game_state):
+        '''Function checks utility value of a state'''
+        pass
+
+    def validate_move(self):
+        '''Function to check if move is valid or not'''
+        pass
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
